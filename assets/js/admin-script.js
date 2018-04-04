defe3401def3bea1a1add2ff840bebb32d7b95a2
asabagh <a.sabagh72@gmail.com>
2018-04-04 06:45:53 +0430

    adding assets folder

---
+++ assets/js/admin-script.js
@@ -0,0 +1,15 @@
+jQuery(document).ready(function ($) {
+    //====================================radio and checkbox switcher
+    $(".cb-enable").click(function () {
+        var parent = $(this).parents('.switch');
+        $('.cb-disable', parent).removeClass('selected');
+        $(this).addClass('selected');
+        $('.checkbox', parent).attr('checked', true);
+    });
+    $(".cb-disable").click(function () {
+        var parent = $(this).parents('.switch');
+        $('.cb-enable', parent).removeClass('selected');
+        $(this).addClass('selected');
+        $('.checkbox', parent).attr('checked', false);
+    });
+});
