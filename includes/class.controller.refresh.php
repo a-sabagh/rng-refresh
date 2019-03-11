6f40b2ef55f7766926f7fa618751abbe7fc8cb17
asabagh <a.sabagh72@gmail.com>
2019-03-11 18:24:12 +0330

    adding is_allowed_refresh and is_active_refresh methods

--- includes/class.controller.refresh.php
+++ includes/class.controller.refresh.php
@@ -14,15 +14,40 @@ class refresh_controller {
     }
 
     /**
+     * check is refresh action allowed or not
+     * @param Integer $post_id
+     * @return boolean
+     */
+    public function is_allowed_refresh($post_id) {
+        $post_type = get_post_type($post_id);
+        $option = get_option("refresh_general_setting_option");
+        $allowed_post_type = (isset($option) and ! empty($option)) ? $option['refresh-active-post-type'] : array('page');
+
+        return in_array($post_type, $allowed_post_type);
+    }
+
+    /**
+     * check is refresh action active or not
+     * @param type $post_id
+     * @return boolean
+     */
+    public function is_active_refresh($post_id) {
+        $is_allowed_refresh = $this->is_allowed_refresh($post_id);
+        $is_refresh = get_post_meta($post_id, "rngrf_is_refresh_active", TRUE);
+        return ($is_allowed_refresh and $is_refresh);
+    }
+
+    /**
      * add refresh meta tag if allowed
      * @global Object $post
      */
     public function refresh_public() {
         global $post;
-        $post_id = $post->ID;
-        $is_refresh = get_post_meta($post_id, "rngrf_is_refresh_active", TRUE);
-        $refresh_time = get_post_meta($post_id, "rngrf_refresh_time", TRUE);
-        if ($is_refresh) {
+        $post_id = (int) $post->ID;
+        $active_refresh = $this->is_active_refresh($post_id);
+        
+        if ($active_refresh) {
+            $refresh_time = get_post_meta($post_id, "rngrf_refresh_time", TRUE);
             echo '<meta http-equiv="refresh" content="' . $refresh_time . '">';
         }
     }
@@ -31,18 +56,9 @@ class refresh_controller {
      * initialize refresh metabox in admin panel
      */
     public function metaboxes_init() {
-        $option = get_option("refresh_general_setting_option");
-        $active_flag = FALSE;
-        if (isset($option)) {
-            if (!empty($option['refresh-active-post-type'])) {
-                $post_types = $option['refresh-active-post-type'];
-                $active_flag = TRUE;
-            }
-        } else {
-            $post_types = array('page');
-            $active_flag = TRUE;
-        }
-        if ($active_flag) {
+        $post_id = intval($_GET['post']);
+
+        if ($this->is_allowed_refresh($post_id)) {
             add_meta_box("refresh_init", esc_html__("Refresh Settings", "rng-refresh"), array($this, 'refresh_metabox_input'), $post_types, "side", "low");
         }
     }
@@ -90,14 +106,14 @@ class refresh_controller {
      * @param Integer $post_id
      */
     public function metaboxes_save($post_id) {
-        
+
         $refresh_time = sanitize_text_field($_POST['rngrf_refresh_time']);
         $refresh_active = sanitize_text_field($_POST['rngrf_is_refresh_active']);
-        
+
         $is_autosave = wp_is_post_autosave($post_id);
         $is_revision = wp_is_post_revision($post_id);
         $is_valid_nonce = (isset($_POST['rng_refresh']) && wp_verify_nonce($_POST['rng_refresh'], basename(__FILE__))) ? TRUE : FALSE;
-        
+
         if (!$is_autosave || !$is_revision || $is_valid_nonce) {
             $refresh_time = $this->splite_second($refresh_time);
             update_post_meta($post_id, 'rngrf_is_refresh_active', $refresh_active);
